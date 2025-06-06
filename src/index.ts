11cb288def2f799ff31c68f83d112da81813eb41
Ngoc Van Tran <van.tran@atolls.com>
2025-06-06 12:39:05 +0000

    Uses tsc to support decorators

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
+import "reflect-metadata";
+
 import { DataSource } from "typeorm";
+import { User } from "./user";
 
 (async () => {
   const dataSource = new DataSource({
     type: "better-sqlite3",
     database: "./data.db",
+    synchronize: true,
+    entities: [User],
   });
 
   await dataSource.initialize();
